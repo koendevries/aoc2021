1a221bbf25a8ddab149291fa2f4bcab730bcbf23
koendevries <koen.devries@persgroep.net>
2021-12-01 15:58:10 +0100

    day 1 in kotlin: clean up

--- app/src/main/kotlin/com/koendevries/aoc2021/Reader.kt
+++ app/src/main/kotlin/com/koendevries/aoc2021/Reader.kt
@@ -2,10 +2,10 @@ package com.koendevries.aoc2021
 
 import java.io.File
 
-data class Assignment(val day: Int, val part: AssignmentPart) {
+data class Assignment(val day: Int, val part: Part) {
     override fun toString(): String = "$day${part.toString().lowercase()}"
 }
 
-enum class AssignmentPart { A, B }
+enum class Part { A, B }
 
 fun File(assignment: Assignment) = File("src/test/resources/${assignment}.txt")
--- app/src/test/kotlin/com/koendevries/aoc2021/day1/Day1.kt
+++ app/src/test/kotlin/com/koendevries/aoc2021/day1/Day1.kt
@@ -1,27 +1,27 @@
 package com.koendevries.aoc2021.day1
 
 import com.koendevries.aoc2021.Assignment
-import com.koendevries.aoc2021.AssignmentPart
 import com.koendevries.aoc2021.File
+import com.koendevries.aoc2021.Part
 import org.junit.Test
 
 
 class Day1 {
-    private val numbers = File(Assignment(1, AssignmentPart.A))
+    private val numbers = File(Assignment(1, Part.A))
         .readLines()
         .map(String::toInt)
 
     @Test
     fun `should solve 1a`() {
         numbers.windowed(2)
-            .count { it[1] > it[0] }
+            .count { it.last() > it.first() }
             .also(::println)
     }
 
     @Test
     fun `should solve 1b`() {
         numbers.windowed(4)
-            .count { it[3] > it[0] }
+            .count { it.last() > it.first() }
             .also(::println)
     }
 
