0f62d2c6ed2949d11c9a04e11ee11760c4a6301f
koendevries <koen.devries@persgroep.net>
2021-12-14 15:41:54 +0100

    day 10 in Kotlin: more fp style

--- app/src/test/kotlin/com/koendevries/aoc2021/day10/Day10.kt
+++ app/src/test/kotlin/com/koendevries/aoc2021/day10/Day10.kt
@@ -1,75 +1,49 @@
 package com.koendevries.aoc2021.day10
 
 import com.koendevries.aoc2021.collections.extensions.median
-import com.koendevries.aoc2021.collections.extensions.swap
 import com.koendevries.aoc2021.io.Assignment
 import com.koendevries.aoc2021.io.File
 import com.koendevries.aoc2021.io.Part
 import org.junit.Test
-import kotlin.math.max
 
 class Day10 {
     private val lines = File(Assignment(10, Part.A)).readLines()
+
     private val openCharByClosingChar = mapOf(')' to '(', ']' to '[', '}' to '{', '>' to '<')
-    private val closingCharByOpenChar = openCharByClosingChar.swap()
     private val pointsByIllegalChar = mapOf(')' to 3, ']' to 57, '}' to 1197, '>' to 25137)
-    private val pointsByMissingChar = mapOf(')' to 1, ']' to 2, '}' to 3, '>' to 4)
+    private val pointsByOpening = mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)
 
     @Test
     fun `should solve 10a`() {
-        lines.mapNotNull(::firstIllegalChar)
+        lines.map(this::toChunkLine)
+            .filterIsInstance<IllegalCharacter>()
+            .map(IllegalCharacter::char)
             .sumOf(pointsByIllegalChar::getValue)
             .also(::println)
     }
 
     @Test
     fun `should solve 10b`() {
-        lines.filterNot(::hasIllegalChar)
-            .map(::missingChars)
-            .map(::toScore)
+        lines.map(this::toChunkLine)
+            .filterIsInstance<Incomplete>()
+            .map(this::toScore)
             .run(Iterable<Long>::median)
             .also(::println)
     }
 
-    private fun toScore(missingChars: String, score: Long = 0L): Long = if (missingChars.isEmpty()) {
-        score
-    } else {
-        toScore(
-            missingChars = missingChars.drop(1),
-            score = score * 5 + pointsByMissingChar[missingChars.first()]!!
-        )
-    }
+    private fun toScore(incomplete: Incomplete) = incomplete.incompletes.fold(0L) { acc, next -> 5 * acc + pointsByOpening.getOrDefault(next, 0) }
 
-    private fun missingChars(incompleteLine: String): String {
-        val closings = openCharByClosingChar.keys
-        val closing = incompleteLine.firstOrNull(closings::contains)
-        return if (closing != null) {
-            val closingIndex = incompleteLine.indexOf(closing)
-            missingChars(incompleteLine.substring(0, closingIndex - 1) + incompleteLine.substring(closingIndex + 1))
-        } else {
-            incompleteLine
-                .reversed()
-                .map(closingCharByOpenChar::getValue)
-                .joinToString("")
-        }
-    }
 
-    private fun hasIllegalChar(line: String) = firstIllegalChar(line) != null
-
-    private fun firstIllegalChar(line: String): Char? {
-        val closings = openCharByClosingChar.keys
-        val closing = line.firstOrNull(closings::contains)
-        return if (closing == null) {
-            null
-        } else {
-            val closingIndex = line.indexOfFirst(closings::contains)
-            val openingIndex = max(0, closingIndex - 1)
-            val previous = line[openingIndex]
-            if (openCharByClosingChar[closing] == previous) {
-                firstIllegalChar(line.substring(0, openingIndex) + line.substring(closingIndex + 1))
-            } else {
-                closing
-            }
+    private fun toChunkLine(line: String): ChunkLine = line.fold(Incomplete(emptyList()) as ChunkLine) { acc, next ->
+        when {
+            acc is Incomplete && openCharByClosingChar.values.contains(next) -> Incomplete(listOf(next) + acc.incompletes)
+            acc is Incomplete && openCharByClosingChar[next] == acc.incompletes.firstOrNull() -> Incomplete(acc.incompletes.drop(1))
+            acc is IllegalCharacter -> acc
+            else -> IllegalCharacter(next)
         }
     }
 }
+
+private sealed interface ChunkLine
+private data class IllegalCharacter(val char: Char) : ChunkLine
+private data class Incomplete(val incompletes: List<Char>) : ChunkLine
