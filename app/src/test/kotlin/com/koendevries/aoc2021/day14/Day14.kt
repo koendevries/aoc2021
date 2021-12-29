1e2bee93e57160d34c1536169b9474aa8736faf3
koendevries <koen.devries@persgroep.net>
2021-12-29 01:23:48 +0100

    day 14 in Kotlin: part b

--- app/src/test/kotlin/com/koendevries/aoc2021/day14/Day14.kt
+++ app/src/test/kotlin/com/koendevries/aoc2021/day14/Day14.kt
@@ -4,38 +4,57 @@ import com.koendevries.aoc2021.io.Assignment
 import com.koendevries.aoc2021.io.File
 import com.koendevries.aoc2021.io.Part
 import org.junit.Test
+import java.util.Collections.max
+import java.util.Collections.min
+
+private typealias PairCounter = Map<String, Long>
+private typealias CharCounter = Map<Char, Long>
+private typealias Rules = Map<String, Char>
+private typealias State = Pair<PairCounter, CharCounter>
 
 class Day14 {
 
     private val input = File(Assignment(14, Part.A)).readText()
         .split("\n\n")
-        .let { (template, rule) -> template to readRules(rule) }
-
 
     @Test
     fun `should solve 14a`() {
-        val (template, rules) = input
-        generateSequence(template) { t -> insert(t, rules) }
-            .drop(10)
-            .first()
-            .let {
-                val charToCount = it.toCharArray()
-                    .distinct()
-                    .associateWith { c -> it.toCharArray().count(c::equals) }
-                it.maxOf(charToCount::getValue) - it.minOf(charToCount::getValue)
-            }
-            .also(::println)
+        charCounts(10).run { max(values) - min(values) }.also(::println)
     }
 
-    private fun insert(template: String, rules: Map<String, String>) = template
-        .windowed(2)
-        .map { pair -> rules[pair] ?: pair }
-        .reduce { acc, next -> acc.dropLast(1) + next }
+    @Test
+    fun `should solve 14b`() {
+        charCounts(40).run { max(values) - min(values) }.also(::println)
+    }
 
-    private fun readRules(text: String) = text.lines()
-        .map(::readRule)
-        .associate { (pair, toInsert) -> pair to (pair.first() + toInsert + pair.last()) }
+    private fun charCounts(iterations: Int): CharCounter {
+        val rules = input[1].lines().map { it.split(" -> ") }.associate { (k, v) -> Pair(k, v[0]) }
+        val pairCounter = input[0].windowed(2).groupBy { it }.mapValues { (_, k) -> k.size.toLong() }
+        val charCounter = input[0].groupBy { it }.mapValues { (_, k) -> k.size.toLong() }
 
+        return generateSequence(Pair(pairCounter, charCounter)) { state -> nextState(state, rules) }
+            .drop(iterations).first().second
+    }
 
-    private fun readRule(line: String) = line.split(" -> ")
+    private fun nextState(state: State, rules: Rules): State {
+        val (pairCounter, charCounter) = state
+        return pairCounter.entries
+            .fold(
+                Pair(
+                    pairCounter.toMutableMap(),
+                    charCounter.toMutableMap()
+                )
+            ) { (pairCounter, charCounter), (pair, count) ->
+                val charToInsert = rules[pair]!!
+                val left = pair.substring(0, 1) + charToInsert
+                val right = charToInsert + pair.substring(1)
+
+                pairCounter.merge(pair, count, Long::minus)
+                pairCounter.merge(left, count, Long::plus)
+                pairCounter.merge(right, count, Long::plus)
+                charCounter.merge(charToInsert, count, Long::plus)
+
+                Pair(pairCounter, charCounter)
+            }
+    }
 }
