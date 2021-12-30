db5734c2ac1908c79150ecf1a9f47d8b8558f34f
koendevries <koen.devries@persgroep.net>
2021-12-30 12:51:20 +0100

    day 15 in Kotlin: part b + optimizations

--- app/src/test/kotlin/com/koendevries/aoc2021/day15/Day15.kt
+++ app/src/test/kotlin/com/koendevries/aoc2021/day15/Day15.kt
@@ -5,53 +5,65 @@ import com.koendevries.aoc2021.io.Assignment
 import com.koendevries.aoc2021.io.File
 import com.koendevries.aoc2021.io.Part
 import org.junit.Test
-import kotlin.math.min
+import java.util.*
+
+private typealias Grid = Map<Point, Int>
+private typealias WeightedPoint = Pair<Point, Int>
+
+private fun Grid.neighbours(point: Point) = directions.map(point::plus).filter(this::contains)
+private val directions = listOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
 
 class Day15 {
 
-    private val input = File(Assignment(15, Part.A)).readLines()
-        .map { row -> row.map { cost -> cost.digitToInt() } }
-    private val directions = listOf(
-        Point(-1, 0),
-        Point(1, 0),
-        Point(0, -1),
-        Point(0, 1)
-    )
+    private val input = File(Assignment(15, Part.A)).readLines().map { row -> row.map { cost -> cost.digitToInt() } }
+    private val source = Point(0, 0)
+    private val grid: Grid = input.flatMapIndexed { y, row -> row.mapIndexed { x, cost -> Point(x, y) to cost } }.toMap()
 
     @Test
     fun `should solve 15a`() {
-        val target = Point(input.first().size - 1, input.size - 1)
-        val costs = input
-            .flatMapIndexed { y, row -> row.mapIndexed { x, cost -> Point(x, y) to cost } }
-            .associate { (point, cost) -> point to cost }
-
-        generateSequence(State()) { state -> nextState(nextVisitingCosts(state, costs)) }
-            .takeWhile { state -> !state.hasVisited(target) }.last()
-            .also { state -> println(state.minByVisited[target]) }
+        val target = grid.keys.maxByOrNull { point -> point.x * point.y } ?: source
+        println(shortestPath(source, target, grid))
     }
 
-    private fun nextState(state: State): State {
-        state.costByVisiting.entries
-            .reduce() { acc, next -> if (acc.value < next.value) acc else next }
-            .also { (point, cost) -> state.minByVisited[point] = cost }
-            .also { (point, _) -> state.costByVisiting.remove(point) }
-            .also { (point, _) -> state.current = point }
-        return state
+    @Test
+    fun `should solve 15b`() {
+        val expandedGrid = expand(grid, 5)
+        val target = expandedGrid.keys.maxByOrNull { point -> point.x * point.y } ?: source
+        println(shortestPath(source, target, expandedGrid))
     }
 
-    private fun nextVisitingCosts(state: State, costs: Map<Point, Int>): State {
-        val (current, minByVisited, costByVisiting) = state
-        return current.neighbours(directions)
-            .map { point -> Pair(point, costs[point]?.plus(minByVisited[current]!!) ?: Int.MAX_VALUE) }
-            .onEach { (point, cost) -> costByVisiting.merge(point, cost, ::min) }
-            .let { state }
+    private fun expand(grid: Grid, times: Int): Grid {
+        val target = grid.keys.maxByOrNull { point -> point.x * point.y }
+        val width = target?.x?.plus(1) ?: 0
+        val height = target?.y?.plus(1) ?: 0
+
+        return List(times) { x: Int ->
+            List(times) { y ->
+                grid.map { (point, weight) ->
+                    Pair(
+                        Point(x = x * width + point.x, y = y * height + point.y),
+                        (1 + (weight - 1 + x + y) % 9)
+                    )
+                }
+            }
+        }.flatten().flatten().toMap()
     }
 
-    private data class State(
-        var current: Point = Point(0, 0),
-        val minByVisited: MutableMap<Point, Int> = mutableMapOf(Point(0, 0) to 0),
-        val costByVisiting: MutableMap<Point, Int> = mutableMapOf()
-    ) {
-        fun hasVisited(point: Point) = minByVisited.keys.contains(point)
+    private fun shortestPath(source: Point, target: Point, grid: Grid): Int {
+        val weightedPoints = PriorityQueue<WeightedPoint> { p1, p2 -> p1.second - p2.second }.apply { offer(WeightedPoint(source, 0)) }
+        val costSoFar = mutableMapOf(source to 0)
+
+        while (weightedPoints.isNotEmpty()) {
+            val (point, _) = weightedPoints.poll()
+
+            for (neighbour in grid.neighbours(point)) {
+                val weight = costSoFar[point]!! + grid[neighbour]!!
+                if (neighbour !in costSoFar || costSoFar[neighbour]!! > weight) {
+                    costSoFar[neighbour] = weight
+                    weightedPoints.offer(WeightedPoint(neighbour, weight))
+                }
+            }
+        }
+        return costSoFar[target]!!
     }
 }
