de593e7fd199dc050288f9abcd88df5c1ec8f745
koendevries <koen.devries@persgroep.net>
2022-01-02 00:23:20 +0100

    remove unused function

--- app/src/main/kotlin/com/koendevries/aoc2021/io/readers/DiagnosticReportReader.kt
+++ app/src/main/kotlin/com/koendevries/aoc2021/io/readers/DiagnosticReportReader.kt
@@ -4,7 +4,3 @@ import com.koendevries.aoc2021.io.File
 import com.koendevries.aoc2021.io.util.Input
 
 fun readDiagnosticReport(input: Input) = File(input).readLines()
-
-fun readDiagnosticRep(input: Input) = File(input).readLines()
-    .map(String::toCharArray)
-    .toTypedArray()
