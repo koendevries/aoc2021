cb93eca3664cfbe38f1fdfe79b495560d4a197d0
koendevries <koen.devries@persgroep.net>
2022-01-01 23:23:10 +0100

    add example day 3

--- app/src/test/kotlin/com/koendevries/aoc2021/solutions/BinaryDiagnosticTest.kt
+++ app/src/test/kotlin/com/koendevries/aoc2021/solutions/BinaryDiagnosticTest.kt
@@ -1,11 +1,22 @@
 package com.koendevries.aoc2021.solutions
 
 import com.koendevries.aoc2021.io.readers.readDiagnosticReport
+import com.koendevries.aoc2021.io.util.ExampleInput
 import com.koendevries.aoc2021.io.util.StandardInput
 import io.kotest.core.spec.style.ShouldSpec
 import io.kotest.matchers.shouldBe
 
 object BinaryDiagnosticTest : ShouldSpec({
+
+    context("example") {
+        val diagnosticReport = readDiagnosticReport(ExampleInput(3))
+        should("calculate power consumption") {
+            powerConsumption(diagnosticReport) shouldBe 198
+        }
+        should("calculate life support rating") {
+            lifeSupportRating(diagnosticReport) shouldBe 230
+        }
+    }
 
     context("standard") {
         val diagnosticReport = readDiagnosticReport(StandardInput(3))
