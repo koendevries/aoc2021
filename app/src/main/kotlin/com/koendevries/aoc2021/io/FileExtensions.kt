79d23a852255f8d3f86c9908852f73aa2ba344d7
koendevries <koen.devries@persgroep.net>
2022-01-01 14:53:05 +0100

    change input folder structure

--- app/src/main/kotlin/com/koendevries/aoc2021/io/FileExtensions.kt
+++ app/src/main/kotlin/com/koendevries/aoc2021/io/FileExtensions.kt
@@ -5,10 +5,9 @@ import com.koendevries.aoc2021.io.util.Input
 import com.koendevries.aoc2021.io.util.InputExample
 import java.io.File
 
-fun File(aocInput: AocInput): File {
-    val name = when (aocInput) {
-        is Input -> aocInput.day.toString()
-        is InputExample -> aocInput.day.toString() + "example"
+fun File(aocInput: AocInput) = File(
+    when (aocInput) {
+        is Input -> "src/test/resources/2021/${aocInput.day}.txt"
+        is InputExample -> "src/test/resources/2021/example/${aocInput.day}.txt"
     }
-    return File("src/test/resources/${name}.txt")
-}
+)
