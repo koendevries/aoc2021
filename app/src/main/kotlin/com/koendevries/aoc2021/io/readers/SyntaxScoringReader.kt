022d989bada290c88795d36cbacad6737c893114
koendevries <koen.de.vries@outlook.com>
2022-01-11 08:55:14 +0100

    refactor day 10

--- app/src/main/kotlin/com/koendevries/aoc2021/io/readers/SyntaxScoringReader.kt
+++ app/src/main/kotlin/com/koendevries/aoc2021/io/readers/SyntaxScoringReader.kt
@@ -2,41 +2,48 @@ package com.koendevries.aoc2021.io.readers
 
 import com.koendevries.aoc2021.io.File
 import com.koendevries.aoc2021.io.util.Input
-import com.koendevries.aoc2021.solutions.Chunks
-import com.koendevries.aoc2021.solutions.CorrectChunks
-import com.koendevries.aoc2021.solutions.CorruptedChunks
-import com.koendevries.aoc2021.solutions.IncompleteChunks
+import com.koendevries.aoc2021.solutions.*
+import java.lang.IllegalStateException
 
 fun readChunkLine(input: Input) = File(input)
     .readLines()
     .map(::chunks)
 
-private fun chunks(line: String): Chunks = line.fold(CorrectChunks as Chunks) { chunks, char ->
-    when (chunks) {
-        is CorrectChunks -> chunks.next(char)
-        is IncompleteChunks -> chunks.next(char)
-        is CorruptedChunks -> chunks
+private fun chunks(line: String): Chunks = line
+    .map(::readChunkChar)
+    .fold(Complete as Chunks) { chunks, char ->
+        when (chunks) {
+            is Complete -> chunks with char
+            is Incomplete -> chunks with char
+            is Corrupt -> chunks
+        }
+    }
+
+fun readChunkChar(char: Char): ChunkChar = when (char) {
+    '(' -> OpeningChar('(', ')', 1L)
+    '[' -> OpeningChar('[', ']', 2L)
+    '{' -> OpeningChar('{', '}', 3L)
+    '<' -> OpeningChar('<', '>', 4L)
+    ')' -> ClosingChar(')', '(', 3L)
+    ']' -> ClosingChar(']', '[', 57L)
+    '}' -> ClosingChar('}', '{', 1197L)
+    '>' -> ClosingChar('>', '<', 25137L)
+    else -> throw IllegalStateException()
+}
+
+infix fun Complete.with(char: ChunkChar) = when (char) {
+    is OpeningChar -> Incomplete(listOf(char))
+    is ClosingChar -> Corrupt(char)
+}
+
+infix fun Incomplete.with(char: ChunkChar): Chunks = when (char) {
+    is OpeningChar -> Incomplete(opened + char)
+    is ClosingChar -> when {
+        completesWith(char) -> Complete
+        matches(opened.lastOrNull(), char) -> Incomplete(opened.dropLast(1))
+        else -> Corrupt(char)
     }
 }
 
-fun CorrectChunks.next(char: Char) = when {
-    isOpeningChar(char) -> IncompleteChunks(listOf(char))
-    else -> CorruptedChunks(char)
-}
-
-fun IncompleteChunks.next(char: Char): Chunks = when {
-    isOpeningChar(char) -> IncompleteChunks(incomplete + char)
-    isCorrect(char) -> CorrectChunks
-    isMatching(char) -> IncompleteChunks(incomplete.dropLast(1))
-    else -> CorruptedChunks(char)
-}
-
-fun IncompleteChunks.isCorrect(next: Char) = incomplete.size == 1 && isMatching(next)
-fun isOpeningChar(char: Char) = char in "([{<"
-fun IncompleteChunks.isMatching(next: Char) = when (next) {
-    ')' -> '(' == incomplete.lastOrNull()
-    ']' -> '[' == incomplete.lastOrNull()
-    '}' -> '{' == incomplete.lastOrNull()
-    '>' -> '<' == incomplete.lastOrNull()
-    else -> false
-}
+fun Incomplete.completesWith(next: ClosingChar) = opened.size == 1 && matches(opened.lastOrNull(), next)
+fun matches(opening: OpeningChar?, closing: ClosingChar) = opening?.matches == closing.char
--- app/src/main/kotlin/com/koendevries/aoc2021/solutions/SyntaxScoring.kt
+++ app/src/main/kotlin/com/koendevries/aoc2021/solutions/SyntaxScoring.kt
@@ -3,33 +3,25 @@ package com.koendevries.aoc2021.solutions
 import com.koendevries.aoc2021.collections.extensions.median
 
 sealed interface Chunks
-object CorrectChunks : Chunks
-data class IncompleteChunks(val incomplete: List<Char>) : Chunks
-data class CorruptedChunks(val firstIllegalChar: Char) : Chunks
+object Complete : Chunks
+data class Incomplete(val opened: List<OpeningChar>) : Chunks
+data class Corrupt(val firstIllegalChar: ClosingChar) : Chunks
 
-private fun CorruptedChunks.syntaxErrorScore() = when (firstIllegalChar) {
-    ')' -> 3L
-    ']' -> 57L
-    '}' -> 1197L
-    '>' -> 25137L
-    else -> throw IllegalStateException()
-}
+sealed interface ChunkChar
+data class OpeningChar(val char: Char, val matches: Char, val score: Long): ChunkChar
+data class ClosingChar(val char: Char, val matches: Char, val score: Long): ChunkChar
 
-private fun IncompleteChunks.score() = incomplete.foldRight(0L, ::addIncomplete)
+private fun Corrupt.syntaxErrorScore() = firstIllegalChar.score
 
-private fun addIncomplete(opening: Char, score: Long): Long = 5 * score + when (opening) {
-    '(' -> 1L
-    '[' -> 2L
-    '{' -> 3L
-    '<' -> 4L
-    else -> 0L
-}
+private fun Incomplete.score() = opened.foldRight(0L, ::addIncomplete)
+
+private fun addIncomplete(opening: OpeningChar, score: Long): Long = 5 * score + opening.score
 
 fun totalSyntaxErrorScore(lines: List<Chunks>): Long = lines
-    .filterIsInstance<CorruptedChunks>()
-    .sumOf(CorruptedChunks::syntaxErrorScore)
+    .filterIsInstance<Corrupt>()
+    .sumOf(Corrupt::syntaxErrorScore)
 
 fun middleScore(lines: List<Chunks>): Long = lines
-    .filterIsInstance<IncompleteChunks>()
-    .map(IncompleteChunks::score)
+    .filterIsInstance<Incomplete>()
+    .map(Incomplete::score)
     .run(Iterable<Long>::median)
